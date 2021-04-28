29afb4b7bc1f22bdd57c3aec843ea5c630542d31
Jaikishan Bagul <78461084+Script-Kiddie-JKB@users.noreply.github.com>
2021-04-28 13:58:11 +0530

    Update FoldBoundary.cpp

--- Hashing/FoldBoundary.cpp
+++ Hashing/FoldBoundary.cpp
@@ -1,256 +1,262 @@
 #include <iostream>
-
+#include <string>
+#include <cmath>
 using namespace std;
-int choice,size,key,loc;
-int array[1000];
-int len_size,len_key;
-int add;
 
-
+int n,address,key,pair_value,pair_size,counter;
+int *arr;
 
 class FoldBoundary{
- public:
- 
- void menu()
- {
-      cout<<" \nEnter the size of the array "<<endl;
-   cin>>size;
-   for(int i=0;i<size;i++)
-  {
-    array[i]=0;
-  }
-   
-   do{
-     cout<<"\n********* Main Menu *********\n";  
-        cout<<"\nChoose one option from the following list ...\n"; 
-		cout<<" 1-> Insert\n 2-> Display\n 3-> Exit\n 4-> Clear Screen\n";
-       cin>>choice;
-       switch(choice)
-    {
-     case 1:
-         insert_E();
-         break;
-     case 2:
-       display();
-         break;
-     case 3:
-         break;
-     default:
-        cout<<" \nEnter proper choice "<<endl;
-         break;
-     } 
-   }while(choice!=3);
-   
-    
- }
- int len( int temp_ele)
- {
-  int l=0;
-  while (temp_ele!=0)
-  {
-   l++;
-   temp_ele=temp_ele/10; 
-  }
-  return l;
- }
- 
- void insert_E(){
-      
-      cout<<" \nEnter the element "<<endl;
-      cin>>key;
-      if(key==0)
-      {
-       cout<<" \nCan not enter 0 as an element "<<endl;
-       return;
-   }
-   
-   sumProvider(key,size);
-      
-      loc=add%size;
-   foldBoundHash(loc);
-   cout<<" Element successfully Inserted "<<endl;    
- }
- 
- 
- void sumProvider(int k , int s)
- {
-  len_size=len(s);
-  len_key=len(k);
-  int fill;
-  int ext_ele=0;
-  int rev_num=0;
-  int t;
-  int temp[10];
-  int rtae=0;
-  int lim;
-  
-  if(len_size>len_key)
-  {
-    fill=len_size-len_key; 
-  }
-  else{
-    ext_ele=len_key%len_size;
-    if(ext_ele!=0)
-    {
-     fill=len_size-ext_ele;
+    public:
+    FoldBoundary(){
+        cout << "Enter the no of locations:";
+        cin >> n;
+        arr = new int[n];
+        pair_size = to_string(n-1).length();     
+        for (int i = 0; i < n; i++) 
+		{
+            arr[i] = INT_MIN;
+        }
     }
-    
-  }
-  t=0;
-  for( int i=k;i!=0;i=i/10)
-  {
+
    
-   t=i%10;
-   rev_num= (rev_num * 10)+t;
-  }
-  if(fill!=0)
-  {
-  for( int j=0;j<fill;j++)
-  {
-   rev_num=rev_num * 10;
-  } 
-  }
-  int len_rev_num=len(rev_num);
-  lim=len_rev_num/len_size;
-  for(int l=0;l<lim;l++)
-  {
-   if(len_size==1)
-   {
-    t=0;
-    t=rev_num%10;
-    rev_num=rev_num/10;
-   }
-   else if(len_size==2)
-   {
-      t=0;
-      t=rev_num%100;
-      rev_num=rev_num/100;
-   }
-   else if(len_size==3)
-   {
-       t=0;
-      t=rev_num%1000;
-      rev_num=rev_num/1000;
-   }
-   else
-   {
-    return; 
-   }
-   temp[l]=t;
-  }
-        if(len_size!=1)
-  {
-  for(int o=0;o<lim;o++)
-  {
-   if(o==0 || o==(lim-1))
-   {
-    continue;
-   }
-   int laste=0;
-   for(int p=temp[o];p!=0;p=p/10)
-   { 
-       laste=p%10;
-    rtae=(rtae * 10)+laste;
-   }
-   temp[o]=rtae;
-  }
- 
- }
-      
-    add=0;
-     for(int m=0;m<lim;m++)
-   { 
-     t=temp[m];
-     add=add+t;
-   
-   }
-  int len_add=len(add);
-  if(len_add>len_size)
-  {
-   if(len_size==1)
-   {
-    
-    add=add%10;
-   }
-   else if(len_size==2)
-   {
-      
-       add=add%100;
-   }
-   else if(len_size==3)
-   {
-       
-      add=add%1000;
-   }
-   else
-   {
-     
-   }
-  }
- 
- 
- }
- 
- void foldBoundHash(int location)
- {
-  int tloc=location;
-  if(array[location]==0)
-  {
-   array[location]=key;
-  }
-  else
-  {
-   while(array[location]!=0)
-   {
-    ++location;
-    if(location>=size)
-    {
-     location=0;
+    bool check_duplicate(){
+        for(int i=0;i<n;i++){
+            if(key == arr[i]){
+                return true;
+            }
+        }
+        return false;
     }
-    if(location==tloc)
-    {
-     break;
+
+    int get_pair(bool rev,int *val){
+        int temp,rem,pow=1;
+        temp = pair_size;
+        pair_value = 0;
+        while(temp--){
+            rem = *val % 10;
+            if(rev)
+                pair_value = (pair_value*10) + rem;
+            else{
+                pair_value = (rem*pow) + pair_value;  //12345
+                pow *= 10;
+            }
+            *val = *val/10;
+        }
+        return pair_value;
     }
-   }
-   if(array[location]==0)
-   {
-    array[location]=key;
-   }
-   else
-   {
-    if(array[location]!=0 && location==tloc)
-    {
-     cout<<"\nCan't Insert..! Index is full :( "<<endl;
+
+    void hash_function(){
+        if(counter<n){
+            cout << "\nEnter the key value:";
+            cin >> key;
+            if(check_duplicate()){
+                cout << "Duplicate values not allowed!";
+                return;
+            }
+            int no_of_pair,temp=key;
+            address = 0;
+            no_of_pair = ceil((float)to_string(key).length()/(float)pair_size);  //work here
+            for (int i=0;i<no_of_pair;i++){
+                if(i==0 || i==no_of_pair-1){//Boundary pair
+                    address += get_pair(true,&key);
+                }else{
+                    address += get_pair(false,&key);
+                }
+            }
+            if(to_string(address).length() > pair_size){
+                address = get_pair(false,&address);
+            }
+            if(address>=n)
+                address %= n;
+            if(arr[address] != INT_MIN){
+                position();
+            }
+            arr[address] = temp;
+            counter++;
+        }
+        else{
+            cout << "\nHashtable is full!";
+        }
     }
-   }
-  }
- }
- 
- display()
- {
-  bool check=false;
-  for(int n=0;n<size;n++)
-  {
-   if(array[n]==0)
-   {
-    continue;
-   }
-   if(array[n]!=0)
-   {
-    check=true;
-    cout<<" the element |" <<array[n]<< "| is stored in index no "<<n<<endl;
-   }
-  }
-  if(check==false)
-  {
-   cout<< " no elements to display "<<endl;
-  }
- }
+    void position()
+	{
+        while(arr[address] != INT_MIN)
+		{
+            if(address >= n){
+                address = 0;
+                continue;
+            }
+            address++;
+        }
+    }
+
+    void display(){
+        cout << "Address\t" << " Key" << endl;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] != INT_MIN){
+                cout << i << "\t-->\t" << arr[i] << endl;
+            }
+        }
+    }
+
+    void menu(){
+        int choice;
+        do{
+            cout<<"\n*********Main Menu*********\n";  
+        	cout<<"\nChoose one option from the following list ...\n";  
+        	cout<<"\n===============================================\n";  
+			cout << "\n1)Fold Boundary\n2)Display\n3)Exit\nEnter your choice:";
+            cin >> choice;
+            switch (choice) {
+                case 1:
+                    hash_function();
+                    break;
+                case 2:
+                    display();
+                    break;
+                case 3:
+                    break;
+                default:
+                    cout << "Please enter a valid choice!";
+            }
+        } while (choice!=3);
+    }
 };
 
-int main()
-{
- FoldBoundary f;
- f.menu();
- return 0;
+int main() {
+    FoldBoundary fb;
+    fb.menu();
+    return 0;
+}
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int n,address,key,pair_value,pair_size,counter;
+int *arr;
+
+class FoldBoundary{
+    public:
+    FoldBoundary(){
+        cout << "Enter the no of locations:";
+        cin >> n;
+        arr = new int[n];
+        pair_size = to_string(n-1).length();     
+        for (int i = 0; i < n; i++) 
+		{
+            arr[i] = INT_MIN;
+        }
+    }
+
+   
+    bool check_duplicate(){
+        for(int i=0;i<n;i++){
+            if(key == arr[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int get_pair(bool rev,int *val){
+        int temp,rem,pow=1;
+        temp = pair_size;
+        pair_value = 0;
+        while(temp--){
+            rem = *val % 10;
+            if(rev)
+                pair_value = (pair_value*10) + rem;
+            else{
+                pair_value = (rem*pow) + pair_value;  //12345
+                pow *= 10;
+            }
+            *val = *val/10;
+        }
+        return pair_value;
+    }
+
+    void hash_function(){
+        if(counter<n){
+            cout << "\nEnter the key value:";
+            cin >> key;
+            if(check_duplicate()){
+                cout << "Duplicate values not allowed!";
+                return;
+            }
+            int no_of_pair,temp=key;
+            address = 0;
+            no_of_pair = ceil((float)to_string(key).length()/(float)pair_size);  //work here
+            for (int i=0;i<no_of_pair;i++){
+                if(i==0 || i==no_of_pair-1){//Boundary pair
+                    address += get_pair(true,&key);
+                }else{
+                    address += get_pair(false,&key);
+                }
+            }
+            if(to_string(address).length() > pair_size){
+                address = get_pair(false,&address);
+            }
+            if(address>=n)
+                address %= n;
+            if(arr[address] != INT_MIN){
+                position();
+            }
+            arr[address] = temp;
+            counter++;
+        }
+        else{
+            cout << "\nHashtable is full!";
+        }
+    }
+    void position()
+	{
+        while(arr[address] != INT_MIN)
+		{
+            if(address >= n){
+                address = 0;
+                continue;
+            }
+            address++;
+        }
+    }
+
+    void display(){
+        cout << "Address\t" << " Key" << endl;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] != INT_MIN){
+                cout << i << "\t-->\t" << arr[i] << endl;
+            }
+        }
+    }
+
+    void menu(){
+        int choice;
+        do{
+            cout<<"\n*********Main Menu*********\n";  
+        	cout<<"\nChoose one option from the following list ...\n";  
+        	cout<<"\n===============================================\n";  
+			cout << "\n1)Fold Boundary\n2)Display\n3)Exit\nEnter your choice:";
+            cin >> choice;
+            switch (choice) {
+                case 1:
+                    hash_function();
+                    break;
+                case 2:
+                    display();
+                    break;
+                case 3:
+                    break;
+                default:
+                    cout << "Please enter a valid choice!";
+            }
+        } while (choice!=3);
+    }
+};
+
+int main() {
+    FoldBoundary fb;
+    fb.menu();
+    return 0;
 }
